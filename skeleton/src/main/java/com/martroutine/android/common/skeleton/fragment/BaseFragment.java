ce33f5f294ca199c090a0954bef0596c6321bfb7
Vtanathip <tanathip.se@gmail.com>
2014-03-28 23:59:09 +0700

    change to fragment support old version

--- skeleton/src/main/java/com/martroutine/android/common/skeleton/fragment/BaseFragment.java
+++ skeleton/src/main/java/com/martroutine/android/common/skeleton/fragment/BaseFragment.java
@@ -1,7 +1,7 @@
 package com.martroutine.android.common.skeleton.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import com.martroutine.android.common.skeleton.activity.BaseActivity;
 
