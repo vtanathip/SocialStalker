7e992ad1410c6024561216dadab3c23734ea73c4
Vtanathip <tanathip.se@gmail.com>
2014-03-28 23:57:53 +0700

    ViewPagger and their fragment

--- app/src/main/java/com/martroutine/android/socialstalker/adapter/IntroAdapter.java
+++ app/src/main/java/com/martroutine/android/socialstalker/adapter/IntroAdapter.java
@@ -4,15 +4,24 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.martroutine.android.socialstalker.fragment.InstructionFragment;
+import com.martroutine.android.socialstalker.fragment.WelcomeFragment;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MARTROUTINE on 27/03/2014.
  */
 public class IntroAdapter extends FragmentPagerAdapter {
 
-    protected static final ArrayList<Fragment> list = new ArrayList<Fragment>(){
+    public static final String TAG = "socialstalker";
 
+    protected static final List<Fragment> list = new ArrayList<Fragment>() {
+        {
+            add(new WelcomeFragment());
+            add(new InstructionFragment());
+        }
     };
 
 
---
+++ app/src/main/java/com/martroutine/android/socialstalker/fragment/InstructionFragment.java
@@ -0,0 +1,19 @@
+package com.martroutine.android.socialstalker.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.martroutine.android.common.skeleton.fragment.BaseFragment;
+import com.martroutine.android.socialstalker.app.R;
+
+/**
+ * Created by MARTROUTINE on 28/03/2014.
+ */
+public class InstructionFragment extends BaseFragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.viewpagger_instruction, container, false);
+    }
+}
