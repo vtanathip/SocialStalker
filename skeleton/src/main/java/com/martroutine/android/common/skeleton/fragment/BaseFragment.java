e759964f903c0478fee48530fbb7f862262b9926
Vtanathip <tanathip.se@gmail.com>
2014-03-29 12:40:07 +0700

    [Fixed] Problem when change orientation because android will stop and start activity so it will not create object graph yet but fragment need it so `NullPointerExcpetion` is coming to town. Resolved by use method onActivityCreated so it will wait for activity is created and will call inject fragment.

--- skeleton/src/main/java/com/martroutine/android/common/skeleton/fragment/BaseFragment.java
+++ skeleton/src/main/java/com/martroutine/android/common/skeleton/fragment/BaseFragment.java
@@ -9,10 +9,10 @@ import com.martroutine.android.common.skeleton.activity.BaseActivity;
  * Created by MARTROUTINE on 27/03/2014.
  */
 public class BaseFragment extends Fragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
         // Assume that it lives within a BaseActivity host
         ((BaseActivity) getActivity()).inject(this);
     }
